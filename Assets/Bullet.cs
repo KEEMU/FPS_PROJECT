dba03122c218bba95fd774c217be314465b11e9c
KEEMU <goldch009@163.com>
2019-06-09 16:27:59 +0800

    kim test

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//for test by kim
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
